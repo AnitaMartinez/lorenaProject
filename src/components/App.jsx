e6c04acb672a0502cea3eb37dc746abc5d020bd7
LorenaSTN <lorenstn@hotmail.com>
2024-08-18 13:31:41 +0200

    finished layers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,14 @@ import Options from "./Options.jsx";
 
 function App() {
   const [layer, setLayers] = useState({
-    background: "src/images/backgrounds/blue50.png",
-    neck: "src/images/neck/default.png",
-    nose: "src/images/nose.png",
-    mouth: "src/images/mouth/default.png",
-    leg: "src/images/leg/default.png",
-    hair: "src/images/hair/default.png",
-    eyes: "src/images/eyes/default.png",
-    ears: "src/images/ears/default.png",
+    Background: "src/images/backgrounds/blue50.png",
+    Neck: "src/images/neck/default.png",
+    Ears: "src/images/ears/default.png",
+    Eyes: "src/images/eyes/default.png",
+    Nose: "src/images/nose.png",
+    Mouth: "src/images/mouth/default.png",
+    Leg: "src/images/leg/default.png",
+    Hair: "src/images/hair/default.png",
   });
 
   const handleLayerChange = (layer, option) => {
@@ -27,63 +27,116 @@ function App() {
       <h2 className="app__title">Alpaca Generator</h2>
       <section>
         <div className="app__imagepreview">
-          <ImageLayer src={layer.background} />
-          <ImageLayer src={layer.neck} />
-          <ImageLayer src={layer.nose} />
-          <ImageLayer src={layer.mouth} />
-          <ImageLayer src={layer.leg} />
-          <ImageLayer src={layer.hair} />
-          <ImageLayer src={layer.ears} />
-          <ImageLayer src={layer.eyes} />
+          <ImageLayer src={layer.Background} />
+          <ImageLayer src={layer.Neck} />
+          <ImageLayer src={layer.Ears} />
+          <ImageLayer src={layer.Eyes} />
+          <ImageLayer src={layer.Nose} />
+          <ImageLayer src={layer.Mouth} />
+          <ImageLayer src={layer.Leg} />
+          <ImageLayer src={layer.Hair} />
         </div>
 
         <div>
           <Options
-            layer="background"
+            layer="Background"
             options={[
-              "src/images/backgrounds/blue50.png",
-              "src/images/backgrounds/blue60.png",
-              "src/images/backgrounds/blue70.png",
-              "src/images/backgrounds/darkblue30.png",
-              "src/images/backgrounds/darkblue50.png",
-              "src/images/backgrounds/darkblue70.png",
-              "src/images/backgrounds/green50.png",
-              "src/images/backgrounds/green60.png",
-              "src/images/backgrounds/green70.png",
-              "src/images/backgrounds/green80.png",
-              "src/images/backgrounds/grey40.png",
-              "src/images/backgrounds/grey70.png",
-              "src/images/backgrounds/grey80.png",
-              "src/images/backgrounds/red50.png",
-              "src/images/backgrounds/red60.png",
-              "src/images/backgrounds/red70.png",
-              "src/images/backgrounds/yellow50.png",
-              "src/images/backgrounds/yellow60.png",
+              "src/images/Backgrounds/blue50.png",
+              "src/images/Backgrounds/blue60.png",
+              "src/images/Backgrounds/blue70.png",
+              "src/images/Backgrounds/darkblue30.png",
+              "src/images/Backgrounds/darkblue50.png",
+              "src/images/Backgrounds/darkblue70.png",
+              "src/images/Backgrounds/green50.png",
+              "src/images/Backgrounds/green60.png",
+              "src/images/Backgrounds/green70.png",
+              "src/images/Backgrounds/green80.png",
+              "src/images/Backgrounds/grey40.png",
+              "src/images/Backgrounds/grey70.png",
+              "src/images/Backgrounds/grey80.png",
+              "src/images/Backgrounds/red50.png",
+              "src/images/Backgrounds/red60.png",
+              "src/images/Backgrounds/red70.png",
+              "src/images/Backgrounds/yellow50.png",
+              "src/images/Backgrounds/yellow60.png",
             ]}
             current={layer.background}
             onLayerChange={handleLayerChange}
           />
+
           <Options
-            layer="ears"
+            layer={"Neck"}
             options={[
-              "src/images/ears/default.png",
-              "src/images/ears/tilt-backward.png",
-              "src/images/ears/tilt-forward.png",
+              "src/images/neck/default.png",
+              "src/images/neck/bend-backward.png",
+              "src/images/neck/bend-forward.png",
+              "src/images/neck/thick.png",
+            ]}
+            current={layer.Neck}
+            onLayerChange={handleLayerChange}
+          />
+
+          <Options
+            layer="Ears"
+            options={[
+              "src/images/Ears/default.png",
+              "src/images/Ears/tilt-backward.png",
+              "src/images/Ears/tilt-forward.png",
             ]}
             current={layer.ears}
             onLayerChange={handleLayerChange}
           />
           <Options
-            layer="eyes"
+            layer="Eyes"
             options={[
-              "src/images/eyes/default.png",
-              "src/images/eyes/angry.png",
-              "src/images/eyes/naughty.png",
-              "src/images/eyes/panda.png",
-              "src/images/eyes/smart.png",
-              "src/images/eyes/star.png",
+              "src/images/Eyes/default.png",
+              "src/images/Eyes/angry.png",
+              "src/images/Eyes/naughty.png",
+              "src/images/Eyes/panda.png",
+              "src/images/Eyes/smart.png",
+              "src/images/Eyes/star.png",
             ]}
-            current={layer.eyes}
+            current={layer.Eyes}
+            onLayerChange={handleLayerChange}
+          />
+
+          <Options
+            layer="Mouth"
+            options={[
+              "src/images/mouth/default.png",
+              "src/images/mouth/astonished.png",
+              "src/images/mouth/eating.png",
+              "src/images/mouth/laugh.png",
+              "src/images/mouth/tongue.png",
+            ]}
+            current={layer.Mouth}
+            onLayerChange={handleLayerChange}
+          />
+          <Options
+            layer="Leg"
+            options={[
+              "src/images/leg/default.png",
+              "src/images/leg/bubble-tea.png",
+              "src/images/leg/cookie.png",
+              "src/images/leg/game-console.png",
+              "src/images/leg/tilt-backward.png",
+              "src/images/leg/tilt-forward.png",
+            ]}
+            current={layer.Leg}
+            onLayerChange={handleLayerChange}
+          />
+          <Options
+            layer="Hair"
+            options={[
+              "src/images/hair/default.png",
+              "src/images/hair/bang.png",
+              "src/images/hair/curls.png",
+              "src/images/hair/elegant.png",
+              "src/images/hair/fancy.png",
+              "src/images/hair/quiff.png",
+              "src/images/hair/short.png",
+            ]}
+            current={layer.Hair}
             onLayerChange={handleLayerChange}
           />
         </div>
